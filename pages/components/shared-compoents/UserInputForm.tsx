facd965c7ac3b75e422ab26e0f80fff760017293
Congwei Bai <congweibai@gmail.com>
2022-06-04 17:50:18 +0930

    chaneg placeholder

--- pages/components/shared-compoents/UserInputForm.tsx
+++ pages/components/shared-compoents/UserInputForm.tsx
@@ -39,7 +39,7 @@ function UserInputForm({ addNewOption, className }: UserInputFormsProps) {
               required
               value={option}
               onChange={(e) => setOption(e.target.value)}
-              placeholder='KFC?'
+              placeholder='eg: KFC?'
               className='appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'
             />
             <button
